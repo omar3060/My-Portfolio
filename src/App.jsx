07e4f10eb13591b4a64019e6df3359c154c7f063
omar mahmoud <omarmahmoudcs@gmail.com>
2025-03-06 03:59:36 +0200

    Fix Project.jsx path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Services from "./components/services/Services";
 import Contact from "./components/contact/Contact";
 import Home from "./components/home/Home";
 import Nav from "./components/nav/Nav";
-import Projects from "./components/projects/projects";
+import Projects from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
 import Footer from "./components/footer/Footer";
 function App() {
